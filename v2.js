fe037c2d760ed885f6e620c612b0b4483cb2781c
miftahul munir <miftahulmunir001@gmail.com>
2023-02-16 16:42:52 +0700

    Update v2.js

--- v2.js
+++ v2.js
@@ -20,8 +20,7 @@ const tera={
 		const nodes=[]
 		var id=0;
 		tera.loader({a:`data/${a}.json`, b:a=>{
-			a=JSON.parse(a);
-			[...new Set(a)].forEach(a=>{
+			[...new Set(JSON.parse(a))].forEach(a=>{
 				tera.lines.push({...tera.nodes.find(b=>b.a==a[0]), lng3:a[2][0], lat3:a[2][1], color:ccol()})
 				tera.lines.push({...tera.nodes.find(b=>b.a==a[1]), color:ccol()})
 				if (!nodes.find(b=>b==a[0])) {
